5c8a80ef1a3369e6d9b425d6aae7c800b5f601fd
Nate <nate.burb@gmail.com>
2019-09-22 18:05:48 -0700

    Integrated ChickenLib for TestBot

--- TeamCode/src/main/java/chickenados/BlankChickenlibTeleop.java
+++ TeamCode/src/main/java/chickenados/BlankChickenlibTeleop.java
@@ -4,20 +4,28 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import chickenados.testbot.CknTestBot;
 import chickenlib.CknTaskManager;
 import chickenlib.util.CknUtil;
 
-@Disabled
+
 @TeleOp(name = "Blank Cknlib TeleOp")
 public class BlankChickenlibTeleop extends LinearOpMode {
 
     CknTaskManager mgr = new CknTaskManager();
 
+    CknTestBot robot;
+
     @Override
     public void runOpMode(){
 
+        robot = new CknTestBot(hardwareMap, telemetry, false);
+
+        robot.dashboard.setLine(0, "Robot Initialized!");
 
         waitForStart();
+
+        robot.dashboard.setLine(1, "Robot Running");
 
         while(opModeIsActive()){
             CknUtil.CknLoopCounter.getInstance().loop++;
@@ -25,6 +33,8 @@ public class BlankChickenlibTeleop extends LinearOpMode {
 
             // Put teleop code below this line
 
+
+            robot.driveBase.tankDrive(gamepad1.left_stick_y, gamepad1.right_stick_y);
 
 
 
