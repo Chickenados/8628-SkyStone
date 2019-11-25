20d344c2a196f28607b66465951efb095cdf8d51
Nate <nate.burb@gmail.com>
2019-11-24 17:41:15 -0800

    Starting on robot stuff

--- TeamCode/src/main/java/skybot/Skybot.java
+++ TeamCode/src/main/java/skybot/Skybot.java
@@ -1,4 +1,19 @@
 package skybot;
 
-public class Skybot {
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import chickenlib.opmode.CknRobot;
+
+public class Skybot extends CknRobot {
+
+    public Skybot(HardwareMap hwMap, Telemetry telemetry, boolean useVuforia){
+
+    }
+
+    public Skybot(HardwareMap hwMap, Telemetry telemetry){
+
+    }
+
 }
