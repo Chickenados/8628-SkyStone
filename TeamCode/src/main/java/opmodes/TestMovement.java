142269e458f889710f5527392a0df58e2cad8b24
Nate <nate.burb@gmail.com>
2019-11-24 18:28:20 -0800

    New Opmode

---
+++ TeamCode/src/main/java/opmodes/TestMovement.java
@@ -0,0 +1,14 @@
+package opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import chickenlib.opmode.CknOpMode;
+
+@Autonomous(name = "PID Movement Test")
+public class TestMovement extends CknOpMode {
+
+    public void initRobot(){
+
+    }
+
+}
