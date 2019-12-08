4fac4090d5950d1786d16093196c8e3e1c6bd0db
chharrington <clairehelenharrington@gmail.com>
2019-12-07 21:19:10 -0800

    Auto changes

--- TeamCode/src/main/java/opmodes/SlideToStoneRed.java
+++ TeamCode/src/main/java/opmodes/SlideToStoneRed.java
@@ -2,6 +2,7 @@ package opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import chickenlib.opmode.CknOpMode;
 import chickenlib.opmode.CknRobot;
@@ -9,12 +10,15 @@ import chickenlib.util.CknEvent;
 import chickenlib.util.CknStateMachine;
 import chickenlib.util.CknTimer;
 import tilerunner.Tilerunner;
+import tilerunner.TilerunnerGrabberArm;
 import tilerunner.TilerunnerInfo;
 
-@Autonomous(name = "Blank Autonomous")
-public class BlankAutonomous extends CknOpMode {
+@Autonomous(name = "Slide to stone red")
+public class SlideToStoneRed extends CknOpMode {
 
-    private final String moduleName = "blankAutonomous";
+
+
+    private final String moduleName = "slideToStoneRed";
 
     private enum State{
         EXTEND,
@@ -25,13 +29,16 @@ public class BlankAutonomous extends CknOpMode {
         DRIVE_TO_BRIDGE,
         DROP,
         PARK,
+        RETRACT,
         END;
     }
+
 
     Tilerunner robot;
     CknStateMachine<State> sm;
     CknEvent event;
     CknTimer timer;
+    TilerunnerGrabberArm tilerunnerGrabberArm;
 
     //Called when init button is pressed
     @Override
@@ -88,34 +95,49 @@ public class BlankAutonomous extends CknOpMode {
                     break;
 
                 case STRAIGHT_UP:
-
-                   robot.grabberArm.goToPosition(TilerunnerInfo.GRABBER_AUTO_HEIGHT_ENCODER_COUNT, event, 2.0);
-
+                    //robot.grabberArm.setMode(DcMotor.RunMode)
+                   //robot.grabberArm.lowPosition(event,3.0);
+                    //robot.grabberArm.setMode(DcMotor)
+                    //robot.grabberArm.goToPosition(1600,event, 3.0);
+                    //robot.grabberArm
+                    //timer.set(4.0,event);
+                    //robot.grabberArm.straightUp(event,4.0);
+                    //tilerunnerGrabberArm.lowPosition(event, 4.0);
+                    robot.grabberArm.lowPosition(event,4.0);
                     sm.waitForSingleEvent(event,State.BACK_UP);
                     break;
 
                 case BACK_UP:
 
-                    robot.pidDrive.setTarget(0,-11,-90,event,2.0);
-
+                    robot.pidDrive.setTarget(0,11,-90,event,2.0);
                     sm.waitForSingleEvent(event, State.DRIVE_TO_BRIDGE);
                     break;
+
                 case DRIVE_TO_BRIDGE:
 
-                    robot.pidDrive.setTarget(0,40,-90,event,3.0);
+                    robot.pidDrive.setTarget(0,-55,-90,event,3.0);
                     sm.waitForSingleEvent(event, State.DROP);
                     break;
 
                 case DROP:
 
-                    robot.foundationGrabber.release(event,1.0);
+                    robot.stoneGrabber.setPosition(TilerunnerInfo.STONE_GRABBER_OPEN_POS);
+                    timer.set(2.0, event);
                     sm.waitForSingleEvent(event,State.PARK);
                     break;
 
                 case PARK:
 
-                    robot.pidDrive.setTarget(0,-10,-135,event,1.0);
-                    sm.waitForSingleEvent(event, State.END);
+                    robot.pidDrive.setTarget(0,12,-90,event,3.0);
+                    sm.waitForSingleEvent(event, State.RETRACT);
+                    break;
+
+                case RETRACT:
+
+                    robot.grabberArm.retract(event,3.0);
+                    sm.waitForSingleEvent(event,State.END);
+                    break;
+
                 case END:
                     sm.stop();
                     break;
