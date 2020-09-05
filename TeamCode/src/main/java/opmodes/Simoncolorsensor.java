d0f008031395c8d20503b01cba3af5e1351fd099
chharrington <clairehelenharrington@gmail.com>
2020-09-04 19:50:33 -0700

    First color sensor attempt

---
+++ TeamCode/src/main/java/opmodes/Simoncolorsensor.java
@@ -0,0 +1,38 @@
+package opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Autonomous (name = "Simoncolorsensor")
+public class Simoncolorsensor extends LinearOpMode {
+
+    DcMotor leftWheel;
+    DcMotor rightWheel;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        leftwheel = hardwareMap.dcMotor.get("frontLeft");
+        rightWheel = hardwareMap.dcMotor.get("frontRight");
+        leftWheel.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+
+        leftWheel.setPower(1);
+        rightWheel.setPower(1);
+
+        sleep(2000);
+
+        leftWheel.setPower(0);
+        rightWheel.setPower(0);
+
+        sleep(1000);
+
+        leftWheel.setPower(1);
+        rightWheel.setPower(-1);
+
+
+    }
+}
