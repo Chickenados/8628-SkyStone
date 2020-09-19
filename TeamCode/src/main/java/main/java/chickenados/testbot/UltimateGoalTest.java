bf171d8d1e8264d5df80909a6206caf9ded3ecdb
claireharrington <55068854+claireharrington@users.noreply.github.com>
2020-09-19 16:21:18 -0700

    Testing from my computer

---
+++ TeamCode/src/main/java/main/java/chickenados/testbot/UltimateGoalTest.java
@@ -0,0 +1,18 @@
+package main.java.chickenados.testbot;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class UltimateGoalTest extends OpMode {
+    @Override
+    public void init() {
+
+    }
+    @Override
+    public void loop(){
+        if(gamepad1.left_stick_y < 0){
+            telemetry.addData("Left stick y", gamepad1.left_stick_y);
+        }
+    }
+}
